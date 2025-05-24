813dc76383127547e120635322c2af82ae1b40df
laplasian <nifilimity@gmail.com>
2025-05-24 17:32:27 +0700

    big update

--- Auditor.h
+++ Auditor.h
@@ -10,23 +10,26 @@ public:
     virtual ~Auditor() = default;
 
     enum AccessType {
-        Readonly, ReadWrite, Owner, Prohibited
+        Prohibited = 1 << 0, Readonly = 1 << 1, Write = 1 << 2, Owner = 1 << 3, Addable = 1 << 4, Removable = 1 << 5,
+        ReadWrite = Readonly | Write,
+        FullAccess = Readonly | Write | Addable | Removable,
+        OwnerReadonly = Readonly | Readonly,
+        OwnerWrite = Write | Owner,
     };
 
-    [[nodiscard]] const virtual std::pair < AccessType, int > read(const std::string& key) const = 0;
+    [[nodiscard]] const virtual std::pair < AccessType, int > check_read(const std::string& key) const = 0;
+    [[nodiscard]] const virtual AccessType check_write(const std::string& key, int data) const { return check_read(key).first;} ;
 };
 
 class FullAccess final : public Auditor {
 public:
     FullAccess() = default;
-    [[nodiscard]] const std::pair < AccessType, int > read(const std::string& key) const override {return access_;};
-private:
-    static constexpr std::pair < AccessType, int > access_ {ReadWrite, 0};
+    [[nodiscard]] const std::pair < AccessType, int > check_read(const std::string& key) const override {return {AccessType::FullAccess, 0};};
 };
 
 class Reader final : public Auditor {
 public:
-    [[nodiscard]] const std::pair < AccessType, int > read(const std::string& key) const override {return access_;};
+    [[nodiscard]] const std::pair < AccessType, int > check_read(const std::string& key) const override {return access_;};
 private:
     static constexpr std::pair < AccessType, int > access_ {Readonly, 0};
 };
@@ -34,17 +37,19 @@ private:
 class OwnMap final : public Auditor {
 public:
     explicit OwnMap(const std::map<std::string, int> & spoofed = {}): spoofed(spoofed) {};
-    [[nodiscard]] const std::pair < AccessType, int > read(const std::string& key) const override {
+    [[nodiscard]] const std::pair < AccessType, int > check_read(const std::string& key) const override {
         const auto item = spoofed.find(key);
-        if (item == spoofed.end())
-        {
-            return access_;
-        }
+        if (item == spoofed.end()) return  {Readonly, 0};
         return  {Owner, item->second};
     };
+    [[nodiscard]] const AccessType check_write(const std::string& key, int data) const override {
+        const auto item = spoofed.find(key);
+        if (item == spoofed.end()) return Readonly;
+        spoofed[key] = data;
+        return Write;
+    };
 private:
-    std::map<std::string, int> spoofed;
-    static constexpr std::pair < AccessType, int > access_ {Readonly, 0};
+    mutable std::map<std::string, int> spoofed;
 };
 
 
@@ -52,36 +57,26 @@ class Proxy final : public Auditor {
 public:
     Proxy(const std::vector<std::string>& write_map, const std::vector<std::string>& prohibited_map ):
         write_map_(write_map), prohibited_map_(prohibited_map) {};
-    [[nodiscard]] const std::pair < AccessType, int > read(const std::string& key) const override {
-        for (auto &key_: prohibited_map_) {
-            if (key == key_) return access_prohibited;
-        }
-        for (auto &key_: write_map_) {
-            if (key == key_) return access_readwrite;
-        }
-        return access_readonly;
+    [[nodiscard]] const std::pair < AccessType, int > check_read(const std::string& key) const override {
+        for (auto &key_: prohibited_map_) { if (key == key_) return {Prohibited, 0}; }
+        for (auto &key_: write_map_) { if (key == key_) return {ReadWrite, 0}; }
+        return {Readonly, 0};
     }
+
 private:
     std::vector<std::string> write_map_;
     std::vector<std::string> prohibited_map_;
-    static constexpr std::pair < AccessType, int > access_readonly {Readonly, 0};
-    static constexpr std::pair < AccessType, int > access_readwrite {ReadWrite, 0};
-    static constexpr std::pair < AccessType, int > access_prohibited {Prohibited, 0};
 };
 
 class Slave final : public Auditor {
 public:
     explicit Slave(const std::vector<std::string>& read_access_map): access_map_(read_access_map){};
-    [[nodiscard]] const std::pair < AccessType, int > read(const std::string& key) const override {
-        for (auto &key_: access_map_) {
-            if (key == key_) return access_readonly;
-        }
-        return access_prohibited;
+    [[nodiscard]] const std::pair < AccessType, int > check_read(const std::string& key) const override {
+        for (auto &key_: access_map_) { if (key == key_) return {Readonly, 0}; }
+        return {Prohibited, 0};
     }
 private:
     std::vector<std::string> access_map_;
-    static constexpr std::pair < AccessType, int > access_readonly {Readonly, 0};
-    static constexpr std::pair < AccessType, int > access_prohibited {Prohibited, 0};
 };
 
 #endif //AUDITOR_H
--- ProxiedMap.cpp
+++ ProxiedMap.cpp
@@ -1,40 +1,46 @@
 #include "ProxiedMap.h"
-
 #include <stdexcept>
 
+bool operator==(Auditor::AccessType container, Auditor::AccessType access) {
+    return (container & access) == access;
+}
 
-int ProxiedMap::read(const std::string &key) {
-    const std::pair < Auditor::AccessType, int > info = auditor_.read(key);
-    if (info.first == Auditor::AccessType::Owner)
-    {
-        return info.second;
-    }
-    if (info.first==Auditor::Prohibited)
-    {
-        throw std::runtime_error("Prohibited");
-    }
-    const auto item = data_.find(key);
-    if (item == data_.end())
-    {
-        throw std::runtime_error("No such key");
-    }
-    return item->second;
+bool operator!=(Auditor::AccessType container, Auditor::AccessType access) {
+    return !(container == access);
+}
+
+int ProxiedMap::read(const std::string &key) const {
+    const std::pair < Auditor::AccessType, int > info = auditor_.check_read(key);
+    if (info.first==Auditor::Prohibited) throw std::runtime_error("Prohibited");
+
+    if (info.first == Auditor::AccessType::Owner) return info.second;
+
+    return data_.at(key);
 }
 
 void ProxiedMap::edit(const std::string &key, int data) {
-    std::pair < Auditor::AccessType, int > info = auditor_.read(key);
-    if (info.first == Auditor::AccessType::Owner)
-    {
-        info.second = data;
-        return;
-    }
-    if (info.first==Auditor::Prohibited || info.first==Auditor::Readonly) {
-        throw std::runtime_error("Prohibited");
-    }
+    Auditor::AccessType info = auditor_.check_write(key, data);
+
+    if (info==Auditor::Prohibited || info!=Auditor::Write) throw std::runtime_error("Prohibited");
+    if (info == (Auditor::AccessType::Owner | Auditor::AccessType::Write)) return;
+
     auto item = data_.find(key);
-    if (item == data_.end())
-    {
-        throw std::runtime_error("No such key");
-    }
-    item->second = data;
+    if (item == data_.end()) return;
+    data_[key]= data;
+}
+
+void ProxiedMap::add(const std::string &key, int data) {
+    Auditor::AccessType info = auditor_.check_read(key).first;
+
+    if (info==Auditor::Prohibited || info!=Auditor::Addable) throw std::runtime_error("Prohibited");
+
+    data_[key] = data;
+}
+
+void ProxiedMap::remove(const std::string &key) {
+    Auditor::AccessType info = auditor_.check_read(key).first;
+
+    if (info==Auditor::Prohibited || info!=Auditor::Removable) throw std::runtime_error("Prohibited");
+
+    data_.erase(key);
 }
